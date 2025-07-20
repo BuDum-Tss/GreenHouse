ccf5d3775d1737b4c5e45e803a0e8b304e3c36dd
trevorTnikes <trevortnikels@gmail.com>
2025-07-20 13:39:56 +0700

    добавлены фото еды

--- frontend/src/components/ProductCard/constants.ts
+++ frontend/src/components/ProductCard/constants.ts
@@ -3,3 +3,157 @@ import ToastImage from '@assets/ToastImage.jpg';
 export const images = {
   1: ToastImage,
 };
+
+import brusketRostCherry from '@assets/brusketRostCherry.jpg';
+
+export const images = {
+  2: brusketRostCherry,
+};
+
+import SalmonСucumberЫandwich from '@assets/SalmonСucumberЫandwich.jpg';
+
+export const images = {
+  3: SalmonСucumberЫandwich,
+};
+
+import BruschettaTomatoBasil from '@assets/BruschettaTomatoBasil.jpg';
+
+export const images = {
+  4: BruschettaTomatoBasil,
+};
+
+import BrusketVeget from '@assets/BrusketVeget.jpg';
+
+export const images = {
+  5:  BrusketVeget,
+}
+
+import PancakeCherriesChocolate from '@assets/PancakeCherriesChocolate.jpg';
+
+export const images = {
+  7:  PancakeCherriesChocolate,
+}
+
+import PancakeHamCheese from '@assets/PancakeHamCheese.jpg';
+
+export const images = {
+  10:  PancakeHamCheese,
+}
+
+import CroisswichTomatoStracciatella from '@assets/CroisswichTomatoStracciatella.jpg';
+
+export const images = {
+  15:  CroisswichTomatoStracciatella,
+}
+
+import KanapeCaesarChicken from '@assets/KanapeCaesarChicken.jpg';
+
+export const images = {
+  30: KanapeCaesarChicken,
+}
+
+
+import FreshlyCoffee from '@assets/FreshlyCoffee.jpg';
+
+export const images = {
+  46: FreshlyCoffee,
+}
+
+import TeaGingerLemon from '@assets/TeaGingerLemon.jpg';
+
+export const images = {
+  51: TeaGingerLemon,
+}
+
+import TeaGingerLemon from '@assets/TeaGingerLemon.jpg';
+
+export const images = {
+  51: TeaGingerLemon,
+}
+
+import CaramelBananaDessert from '@assets/CaramelBananaDessert.jpg';
+
+export const images = {
+  64: CaramelBananaDessert,
+}
+
+import CaesarSaladChicken from '@assets/CaesarSaladChicken.jpg';
+
+export const images = {
+  69: CaesarSaladChicken,
+}
+
+import MiniChickenSourSauce from '@assets/MiniChickenSourSauce.jpg';
+
+export const images = {
+  79: MiniChickenSourSauce,
+}
+
+import BeefPilaf from '@assets/BeefPilaf.jpg';
+
+export const images = {
+  86: BeefPilaf,
+}
+
+import GrilledTongueSpicySauce from '@assets/GrilledTongueSpicySauce.jpg';
+
+export const images = {
+  90: GrilledTongueSpicySauce,
+}
+
+import PancakeMeat from '@assets/PancakeMeat.jpg';
+
+export const images = {
+  100: PancakeMeat,
+}
+
+import СheeseСakes from '@assets/СheeseСakes.jpg';
+
+export const images = {
+  108: СheeseСakes,
+}
+
+import Chops from '@assets/Chops.jpg';
+
+export const images = {
+  111: Chops,
+}
+
+import Chops from '@assets/Chops.jpg';
+
+export const images = {
+  112: Chops,
+}
+import PozharskyCutlets from '@assets/PozharskyCutlets.jpg';
+
+export const images = {
+  113: PozharskyCutlets,
+}
+
+import ChickenCutlets from '@assets/ChickenCutlets.jpg';
+
+export const images = {
+  116: ChickenCutlets,
+}
+
+import LiverFritters from '@assets/LiverFritters.jpg';
+
+export const images = {
+  120: LiverFritters,
+}
+
+import RainbowDumplings from '@assets/RainbowDumplings.jpg';
+
+export const images = {
+  124: RainbowDumplings,
+}
+
+import dumplings from '@assets/пельмени.jpg';
+
+export const images = {
+  125: dumplings,
+}
+
+
+
+
